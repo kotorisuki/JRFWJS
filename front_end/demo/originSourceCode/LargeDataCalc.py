a75a714a849196b091b8bcac40e32d6a7fc9ab5b
wenhanhuang <wenhanhuang@wenhanhuangs-MacBook-Pro.local>
2017-12-27 02:49:39 +0800

    basic front end finish

---
+++ front_end/demo/originSourceCode/LargeDataCalc.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# 读取json文件中的数据，进行批量处理
+
+import json
+from integration import integration
+
+def data_input(filename, resultfilename):
+    with open(filename) as json_file:
+        data = json.load(json_file)
+    cnt = len(data)
+    result=[]
+    for i in xrange(cnt):
+        pv,yy,dd,cc=integration(data[i]['coupon_rate'],
+                                data[i]['face_value'],
+                                data[i]['time_start'],
+                                data[i]['year'],
+                                data[i]['time_cur'],
+                                data[i]['frequency'])
+        re={'Pv':pv,'Yield':yy,'Duration':dd,'Convexity':cc}
+        result.append(re)
+        #if(i%1000 == 0):
+            #print "PV：%f" % pv
+            #print "yield：%f" % yy
+            #print "Duration：%f" % dd
+            #print "Convexity：%f" % cc
+            #print "==============%d============="%i
+    with open(resultfilename, 'w') as json_file:
+            json_file.write(json.dumps(result))
